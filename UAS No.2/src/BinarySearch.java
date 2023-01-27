2383447295ba6ae1b84bbd96fda3276fd2e25b0c
didhanaditya <116304830+didhanaditya@users.noreply.github.com>
2023-01-28 01:08:58 +0700

    Add files via upload

---
+++ UAS No.2/src/BinarySearch.java
@@ -0,0 +1,25 @@
+class BinarySearch{
+    public static void binarySearch(int arr[], int first, int last, int key){
+        int mid = (first + last)/2;
+        while( first <= last ){
+            if ( arr[mid] < key ){
+                first = mid + 1;
+            }else if ( arr[mid] == key ){
+                System.out.println("Element Ditemukan Pada Index Ke : " + mid);
+                break;
+            }else{
+                last = mid - 1;
+            }
+            mid = (first + last)/2;
+        }
+        if ( first > last ){
+            System.out.println("Element Tidak Ditemukan!");
+        }
+    }
+    public static void main(String args[]){
+        int arr[] = {11,22,33,44,55};
+        int key = 22;
+        int last=arr.length-1;
+        binarySearch(arr,0,last,key);
+    }
+}  
